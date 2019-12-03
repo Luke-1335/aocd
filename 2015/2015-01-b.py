8dcdfa26321802733c5a8c922c78e7da07fecfbe
Luke-1335 <58416589+Luke-1335@users.noreply.github.com>
2019-12-03 14:02:17 +0200

    Removed trailing whitespace

--- 2015/2015-01-b.py
+++ 2015/2015-01-b.py
@@ -5,7 +5,7 @@ lvl = 0
 for i, c in enumerate(data, start=1):
     lvl += 1 if c == "(" else -1
     if lvl == -1:
-        print("result", i)  
+        print("result", i)
         submit(i)
         break
 
