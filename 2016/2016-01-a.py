7bf8213b35c9e6aa5339aa1ac3212b3b90b2d3b6
Luke-1335 <58416589+Luke-1335@users.noreply.github.com>
2019-12-03 14:20:16 +0200

    Removed trailing whitespace

--- 2016/2016-01-a.py
+++ 2016/2016-01-a.py
@@ -7,9 +7,9 @@ for instruction in data.split(", "):
     side = 1 if instruction[0] == "R" else -1
     step = int(instruction[1:])
     i = (i + side) % 4
-    dirs[i] += step   
+    dirs[i] += step
 
 result = abs(dirs[0] - dirs[2]) + abs(dirs[1] - dirs[3])
-print("result", result)  
+print("result", result)
 submit(result)
 
