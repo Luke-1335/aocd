43d21c843161bf2e3723c7252cebebd8cc4344ee
Luke-1335 <58416589+Luke-1335@users.noreply.github.com>
2019-12-03 13:50:28 +0200

    Removed trailing whitespaces

--- 2019/2019-03-a.py
+++ 2019/2019-03-a.py
@@ -10,21 +10,21 @@ for i in range(2):
     for val in data[i].split(","):
         dir  = val[0]
         step = int(val[1:])
-        
+
         while step > 0:
-            step -= 1 
+            step -= 1
             if   dir == "U": pos[1] -= 1
             elif dir == "D": pos[1] += 1
             elif dir == "L": pos[0] -= 1
             elif dir == "R": pos[0] += 1
-            
+
             npos = (pos[0], pos[1])
             if i == 1 and npos in seen:
                 intersections.append(npos)
             elif i == 0:
                 seen.add(npos)
-            
+
 result = min([abs(pt[0]) + abs(pt[1]) for pt in intersections])
-print("result", result)  
+print("result", result)
 submit(result)
 
