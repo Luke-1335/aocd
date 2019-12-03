ce0eb09d1ef93b41bcf5483469bd1e2af3034578
Luke-1335 <58416589+Luke-1335@users.noreply.github.com>
2019-12-03 09:22:23 +0200

    Add solutions for year 2019 - day 3

---
+++ 2019/2019-03-a.py
@@ -0,0 +1,30 @@
+from aocd import data, submit
+data = data.split("\n")
+
+
+seen = {(0, 0)}
+intersections = []
+
+for i in range(2):
+    pos = [0, 0]
+    for val in data[i].split(","):
+        dir  = val[0]
+        step = int(val[1:])
+        
+        while step > 0:
+            step -= 1 
+            if   dir == "U": pos[1] -= 1
+            elif dir == "D": pos[1] += 1
+            elif dir == "L": pos[0] -= 1
+            elif dir == "R": pos[0] += 1
+            
+            npos = (pos[0], pos[1])
+            if i == 1 and npos in seen:
+                intersections.append(npos)
+            elif i == 0:
+                seen.add(npos)
+            
+result = min([abs(pt[0]) + abs(pt[1]) for pt in intersections])
+print("result", result)  
+submit(result)
+
---
+++ 2019/2019-03-b.py
@@ -0,0 +1,32 @@
+from aocd import data, submit
+data = data.split("\n")
+
+
+LARGE = 2**128
+seen = {(0, 0): [0, LARGE]}
+
+for i in range(2):
+    pos  = [0, 0]
+    dist = 0
+    for val in data[i].split(","):
+        dir  = val[0]
+        step = int(val[1:])
+        
+        while step > 0:
+            step -= 1
+            dist += 1
+            if   dir == "U": pos[1] -= 1
+            elif dir == "D": pos[1] += 1
+            elif dir == "L": pos[0] -= 1
+            elif dir == "R": pos[0] += 1
+            
+            npos = (pos[0], pos[1])
+            if   i == 1 and npos in seen:
+                seen[npos][1] = min(seen[npos][1], dist)   
+            elif i == 0 and not npos in seen:
+                seen[npos] = [dist, LARGE]
+            
+result = min([sum(dist) for dist in seen.values()])
+print("result", result)  
+submit(result)
+
