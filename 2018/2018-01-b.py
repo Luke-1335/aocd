ef710167224d4602209afd4806f602c5d6453104
Luke-1335 <58416589+Luke-1335@users.noreply.github.com>
2019-12-03 14:27:31 +0200

    Removed trailing whitespace

--- 2018/2018-01-b.py
+++ 2018/2018-01-b.py
@@ -5,7 +5,7 @@ instructions = [int(line) for line in data.split("\n")]
 freq, i = 0, -1
 seen = set()
 
-while freq not in seen: 
+while freq not in seen:
     seen.add(freq)
     i = (i + 1) % len(instructions)
     freq += instructions[i]
