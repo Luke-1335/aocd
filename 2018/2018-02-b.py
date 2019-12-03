4a20bae26a8528994cab4187466f86bfd46adc20
Luke-1335 <58416589+Luke-1335@users.noreply.github.com>
2019-12-03 14:18:35 +0200

    Removed trailing whitespace

--- 2018/2018-02-b.py
+++ 2018/2018-02-b.py
@@ -5,13 +5,13 @@ words = data.split("\n")
 for w1 in words:
     for w2 in words:
         diff, pos = 0, 0
-        
+
         for i in range(len(w1)):
             if w1[i] != w2[i]:
                 diff +=1
                 if diff == 2: break
                 pos = i
-                
+
         if diff == 1:
             result = w1[:pos] + w1[pos+1:]
             print("result", result)
