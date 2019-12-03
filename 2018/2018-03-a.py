e7abbc6c446944816d07b7acd9be3a9efa7093d1
Luke-1335 <58416589+Luke-1335@users.noreply.github.com>
2019-12-03 14:25:46 +0200

    Removed trailing whitespace

--- 2018/2018-03-a.py
+++ 2018/2018-03-a.py
@@ -6,13 +6,13 @@ for fab in data.split("\n"):
     fab = fab.split(" ")
     x, y = map(int, fab[2][:-1].split(","))
     w, t = map(int, fab[3].split("x"))
-    
-    for yy in range(y, y+t): 
+
+    for yy in range(y, y+t):
         for xx in range(x, x+w):
             pt = (xx, yy)
             if pt in suit: suit[pt] += 1
             else:          suit[pt]  = 0
-    
+
 result = sum([n > 0 for n in suit.values()])
 print("result", result)
 submit(result)
