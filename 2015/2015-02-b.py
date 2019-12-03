86e61c2cca04d0361d3779742680f1cf43f31fc0
Luke-1335 <58416589+Luke-1335@users.noreply.github.com>
2019-12-03 14:06:27 +0200

    Removed trailing whitespace

--- 2015/2015-02-b.py
+++ 2015/2015-02-b.py
@@ -6,7 +6,7 @@ lines = data.split("\n")
 result = 0
 for line in lines:
     lengths = [int(val) for val in line.split("x")]
-    result += 2*(sum(lengths) - max(lengths)) + np.prod(lengths) 
+    result += 2*(sum(lengths) - max(lengths)) + np.prod(lengths)
 
 print("result", result)
 submit(result)
