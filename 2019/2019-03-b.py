8bfc0e614f832b759332578ba4d1f6128995b32e
Luke-1335 <58416589+Luke-1335@users.noreply.github.com>
2019-12-03 13:52:49 +0200

    Code refactor - not  npos in -> npos not in

--- 2019/2019-03-b.py
+++ 2019/2019-03-b.py
@@ -23,7 +23,7 @@ for i in range(2):
             npos = (pos[0], pos[1])
             if   i == 1 and npos in seen:
                 seen[npos][1] = min(seen[npos][1], dist)   
-            elif i == 0 and not npos in seen:
+            elif i == 0 and npos not in seen:
                 seen[npos] = [dist, LARGE]
 
 result = min([sum(dist) for dist in seen.values()])
