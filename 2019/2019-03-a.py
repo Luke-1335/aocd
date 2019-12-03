effc80969b746d47ff90f0589e7d8e189c248910
Luke-1335 <58416589+Luke-1335@users.noreply.github.com>
2019-12-03 14:15:25 +0200

    Code refactor - rename dir -> way

--- 2019/2019-03-a.py
+++ 2019/2019-03-a.py
@@ -8,15 +8,15 @@ intersections = []
 for i in range(2):
     pos = [0, 0]
     for val in data[i].split(","):
-        dir  = val[0]
+        way  = val[0]
         step = int(val[1:])
 
         while step > 0:
             step -= 1
-            if   dir == "U": pos[1] -= 1
-            elif dir == "D": pos[1] += 1
-            elif dir == "L": pos[0] -= 1
-            elif dir == "R": pos[0] += 1
+            if   way == "U": pos[1] -= 1
+            elif way == "D": pos[1] += 1
+            elif way == "L": pos[0] -= 1
+            elif way == "R": pos[0] += 1
 
             npos = (pos[0], pos[1])
             if i == 1 and npos in seen:
