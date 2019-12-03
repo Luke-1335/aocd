6a2287aa143db03a0559950d28c26a6b60ee5bad
Luke-1335 <58416589+Luke-1335@users.noreply.github.com>
2019-12-03 13:47:30 +0200

    Removed trailing whitespaces

--- 2019/2019-02-a.py
+++ 2019/2019-02-a.py
@@ -9,11 +9,11 @@ program[2] = 2
 for i in range(0, len(program), 4):
     opcode = program[i]
     if opcode == 99: break
-         
+
     a = program[i+1]
     b = program[i+2]
     c = program[i+3]
-            
+
     if opcode == 1:
         program[c] = program[a] + program[b]
     elif opcode == 2:
@@ -22,7 +22,6 @@ for i in range(0, len(program), 4):
         print("wtf is this opcode?")
 
 result = program[0]
-print("result", result)  
+print("result", result)
 submit(result)
-            
-            
+
