5a45ee5a40ef2c7e4a6c12f540c5ff06b558f8c3
Luke-1335 <58416589+Luke-1335@users.noreply.github.com>
2019-12-03 14:00:05 +0200

    Code refactor - rename id -> f_id

--- 2018/2018-03-b.py
+++ 2018/2018-03-b.py
@@ -5,20 +5,20 @@ suit = {}
 fabrics = set()
 for fab in data.split("\n"):
     fab  = fab.split(" ")
-    id   = int(fab[0].replace("#", ""))
+    f_id = int(fab[0].replace("#", ""))
     x, y = map(int, fab[2][:-1].split(","))
     w, t = map(int, fab[3].split("x"))
-    fabrics.add(id)
-    
+    fabrics.add(f_id)
+
     for yy in range(y, y+t): 
         for xx in range(x, x+w):
             pt = (xx, yy)
             if pt in suit: 
-                suit[pt].add(id)
+                suit[pt].add(f_id)
                 fabrics -= suit[pt]
             else:
-                suit[pt] = {id}
-                                
+                suit[pt] = {f_id}
+
 result = min(fabrics)
 print("result", result)
 submit(result)
