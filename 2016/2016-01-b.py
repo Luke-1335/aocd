790015968fa1dde0762d61cbea729e8754fe191b
Luke-1335 <58416589+Luke-1335@users.noreply.github.com>
2019-12-03 14:21:33 +0200

    Removed trailing whitespace

--- 2016/2016-01-b.py
+++ 2016/2016-01-b.py
@@ -8,12 +8,12 @@ for instruction in data.split(", "):
     side = 1 if instruction[0] == "R" else -1
     step = int(instruction[1:])
     i = (i + side) % 4
-    
+
     while step > 0:
         dirs[i] += 1
         pos = (dirs[0] - dirs[2], dirs[1] - dirs[3])
- 
-        if (pos in visited): 
+
+        if (pos in visited):
             result = abs(dirs[0] - dirs[2]) + abs(dirs[1] - dirs[3])
             print("result", result)
             submit(result)
