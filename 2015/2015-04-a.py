1110a2670704d0b4923d54ec0fc2866c9af51e8a
Luke-1335 <58416589+Luke-1335@users.noreply.github.com>
2019-12-05 22:43:23 +0200

    Add solutions for year 2015 - day 4

---
+++ 2015/2015-04-a.py
@@ -0,0 +1,14 @@
+from aocd import data, submit
+from hashlib import md5
+data = data.encode()
+
+
+i = 0
+for i in range(2**64):
+    val  = data + str(i).encode()
+    hash = md5(val)
+    if hash.hexdigest()[:5] == "00000": break
+
+print("result", i)
+submit(i)
+
