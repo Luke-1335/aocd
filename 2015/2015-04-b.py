5da265c9af2a60068cc95adde0912b3801e365e5
Luke-1335 <58416589+Luke-1335@users.noreply.github.com>
2019-12-05 22:51:06 +0200

    Code refactor - rename variable hash -> hashval

--- 2015/2015-04-b.py
+++ 2015/2015-04-b.py
@@ -6,8 +6,8 @@ data = data.encode()
 i = 0
 for i in range(2**64):
     val  = data + str(i).encode()
-    hash = md5(val)
-    if hash.hexdigest()[:6] == "000000": break
+    hashval = md5(val)
+    if hashval.hexdigest()[:6] == "000000": break
 
 print("result", i)
 submit(i)
