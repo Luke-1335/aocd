f053d112eee66a80ba608b63348731b553a8f5d5
Luke-1335 <58416589+Luke-1335@users.noreply.github.com>
2019-12-08 14:19:23 +0200

    Shorten solution for year 2019 - day 8, part a

--- 2019/2019-08-a.py
+++ 2019/2019-08-a.py
@@ -1,33 +1,15 @@
 from aocd import data, submit
 
 
-zeros  = []
-layers = []
-w, h, z = 25, 6, 0
+ilen = 25 * 6
 
-create_layer = lambda: [[ None for x in range(w)] for y in range(h)]
-layer = create_layer()
+n = [ (data[i:i+ilen].count("0"),
+       data[i:i+ilen].count("1"),
+       data[i:i+ilen].count("2"))
+     for i in range(0, len(data), ilen) ]
 
-for i, c in enumerate(data):
-    if i % (w*h) == 0 and i:
-        layers.append(layer)
-        zeros.append(z)
-        layer, z = create_layer(), 0
-
-    z += c == "0"
-    layer[i//w%h][i%w] = c
-
-layers.append(layer)
-zeros.append(z)
-
-
-min_i = zeros.index(min(zeros)) #another exmaple of an efficiency :D
-ones, twos = 0, 0
-for y in range(h):
-    ones += layers[min_i][y].count("1")
-    twos += layers[min_i][y].count("2")
-
-result = ones * twos
+n.sort()
+result = n[0][1] * n[0][2]
 print("result", result)
 submit(result)
 
