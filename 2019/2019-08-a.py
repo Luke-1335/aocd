9d55afb801c9a87bbc8f313d76a803e00cbad98b
Luke-1335 <58416589+Luke-1335@users.noreply.github.com>
2019-12-08 14:57:07 +0200

    code refactor, using lambda in list comprehension

--- 2019/2019-08-a.py
+++ 2019/2019-08-a.py
@@ -2,11 +2,8 @@ from aocd import data, submit
 
 
 ilen = 25 * 6
-
-n = [ (data[i:i+ilen].count("0"),
-       data[i:i+ilen].count("1"),
-       data[i:i+ilen].count("2"))
-     for i in range(0, len(data), ilen) ]
+get_counts = lambda s: (s.count("0"), s.count("1"), s.count("2"))
+n = [ get_counts(data[i:i+ilen]) for i in range(0, len(data), ilen) ]
 
 n.sort()
 result = n[0][1] * n[0][2]
