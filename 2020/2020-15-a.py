ca7532fb89ea43719f121448cbdf35e5b8c5f9c0
Luke-1335 <58416589+Luke-1335@users.noreply.github.com>
2020-12-15 21:21:08 +0200

    add day 15 solutions

---
+++ 2020/2020-15-a.py
@@ -0,0 +1,18 @@
+from aocd import data, submit
+
+
+data = [int(d) for d in data.split(",")]
+spoken = {number: turn for turn, number in enumerate(data, 1)}
+number = data[-1]
+turn = len(spoken)
+
+while turn < 2020:
+    try: last = spoken[number]
+    except KeyError: last = turn
+    spoken[number] = turn
+    number = turn - last
+    turn += 1
+
+print(number)
+submit(number)
+
